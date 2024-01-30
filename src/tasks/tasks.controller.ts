b0ee33885321a787657355ce6a8d859e3a371004
triusdeil <triusdeil@gmail.com>
2024-01-30 12:32:09 -0400

    create & list tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Put, Post, Delete, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service'
+import {CreateTaskDto} from './dto/task.dto'
 
 @Controller('tasks')
 export class TasksController {
     
+    constructor(private tasksService: TasksService) {}
+
     @Get()
-    helloWorld() {
-        return 'hello world'
+    getAllTasks() {
+        return this.tasksService.getAllTasks()
+    }
+
+    @Post()
+    createTask(@Body() newTask : CreateTaskDto){
+        return this.tasksService.createTasks(newTask.title, newTask.description)
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import {v4} from 'uuid'
+import {Task, TaskStatus} from './task.entity'
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    private tasks: Task[] = [
+        {
+            id: '1',
+            title: 'first task',
+            description: 'some task',
+            status: TaskStatus.PENDING
+        }
+    ]
+
+    getAllTasks() {
+       return this.tasks
+    }
+
+    getTaskById(){
+    }
+
+    createTasks(title: string, description: string) {
+        const task = {
+            id: v4(),
+            title,
+            description,
+            status: TaskStatus.PENDING
+        }   
+        this.tasks.push(task)
+        return task
+    }
+
+    updateTask() {
+
+    }
+
+    deleteTask() {
+
+    }
+
+}
