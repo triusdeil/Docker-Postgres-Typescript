09c95206ffe02c8626cf1050341d6dcdacf5daed
triusdeil <triusdeil@gmail.com>
2024-01-30 18:13:14 -0400

    validations basics

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,28 @@
 import { TaskStatus } from "../task.entity"
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength  } from 'class-validator'
 
 export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string
+
+    @IsString()
+    @IsNotEmpty()
     description: string
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string
+
+    @IsOptional()
+    @IsString()
     description?: string
+
+    @IsOptional()
+    @IsString()
+    @IsIn([TaskStatus.DONE, TaskStatus.PENDING, TaskStatus.IN_PROGRESS])
     status?: TaskStatus
 }
